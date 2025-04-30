18e7838f66434ab252ec646e74b5ca122087ec6f
Yeoeun Yang <yyang78077@naver.com>
2025-05-01 00:44:39 +0900

    init: project 생성

---
+++ src/main/java/com/ureca/juksoon/JuksoonApplication.java
@@ -0,0 +1,15 @@
+package com.ureca.juksoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class JuksoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuksoonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ureca/juksoon/JuksoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ureca.juksoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuksoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
