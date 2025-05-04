4e7a5c1605a6ad847af6d77cde56792427be92e7
chungjeongsu <wjdwlgh2000@gmail.com>
2025-05-05 05:18:52 +0900

    feat: User, RefreshToken 엔티티 구현
    - User의 권한 UserRole(enum)로 표현함
    - RefreshToken 엔티티 구현

---
+++ src/main/java/com/ureca/juksoon/domain/refresh/entity/RefreshToken.java
@@ -0,0 +1,30 @@
+package com.ureca.juksoon.domain.refresh.entity;
+
+import com.ureca.juksoon.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "refresh_token")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "token", nullable = false)
+    private String token;
+
+    @Column(name = "expired_at", nullable = false)
+    private LocalDateTime expiredAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
