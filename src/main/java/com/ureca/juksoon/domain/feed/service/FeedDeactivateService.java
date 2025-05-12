7f50b54e50347785a7130c9153ee64057e5cd61c
chungjeongsu <wjdwlgh2000@gmail.com>
2025-05-13 00:42:12 +0900

    feat: 배치 처리 서비스 추가(구현 미완)

---
+++ src/main/java/com/ureca/juksoon/domain/feed/service/FeedDeactivateService.java
@@ -0,0 +1,12 @@
+package com.ureca.juksoon.domain.feed.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class FeedDeactivateService {
+
+//    @Scheduled()  요구사항 듣고 구현하기
+}
