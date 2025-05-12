d6179981e1055a3635d7c726184fdd4f62c60452
Yeoeun Yang <yyang78077@naver.com>
2025-05-12 15:48:37 +0900

    refactor: feed repository 의존성 수정

--- src/main/java/com/ureca/juksoon/domain/feed/repository/FeedRepository.java
+++ src/main/java/com/ureca/juksoon/domain/feed/repository/FeedRepository.java
@@ -1,11 +1,7 @@
 package com.ureca.juksoon.domain.feed.repository;
 
 import com.ureca.juksoon.domain.feed.entity.Feed;
-import com.ureca.juksoon.domain.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface FeedRepository extends JpaRepository<Feed, Long> {
-    List<Feed> findAllByStore(Store store);
+public interface FeedRepository extends JpaRepository<Feed, Long>, CustomFeedRepository {
 }
