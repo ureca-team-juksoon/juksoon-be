ac410aacc2fdb315b7f26e206859e3288b91aa8e
Yeoeun Yang <yyang78077@naver.com>
2025-05-15 14:15:09 +0900

    refactor: index를 통한 성능 개선

--- src/main/java/com/ureca/juksoon/domain/feed/repository/CustomFeedRepositoryImpl.java
+++ src/main/java/com/ureca/juksoon/domain/feed/repository/CustomFeedRepositoryImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 import static com.ureca.juksoon.domain.feed.entity.QFeed.feed;
+import static com.ureca.juksoon.domain.feed.entity.SortType.PRICE_ASC;
 import static com.ureca.juksoon.domain.reservation.entity.QReservation.reservation;
 import static com.ureca.juksoon.domain.store.entity.QStore.store;
 
@@ -45,9 +46,10 @@ public class CustomFeedRepositoryImpl implements CustomFeedRepository {
      */
     @Override
     public List<Feed> findPageByFiltering(Pageable pageable, boolean isAvailable, SortType sortType, Category category, String keyword) {
-        return jpaQueryFactory
-            .selectFrom(feed)
-            .leftJoin(feed.store, store)
+        // 서브쿼리로 ID 먼저 추출
+        List<Long> feedIds = jpaQueryFactory
+            .select(feed.id)
+            .from(feed)
             .where(
                 isOpen(isAvailable),
                 categoryEq(category),
@@ -56,6 +58,13 @@ public class CustomFeedRepositoryImpl implements CustomFeedRepository {
             .orderBy(getSortTypeSpecifier(sortType))
             .offset(pageable.getOffset())
             .limit(pageable.getPageSize())
+            .fetch();
+
+        // feedId 기반 전체 데이터 조회
+        return jpaQueryFactory
+            .selectFrom(feed)
+            .leftJoin(feed.store, store).fetchJoin()
+            .where(feed.id.in(feedIds))
             .fetch();
     }
 
@@ -86,12 +95,15 @@ public class CustomFeedRepositoryImpl implements CustomFeedRepository {
     }
 
     // 정렬 순서 설정
-    private OrderSpecifier<?> getSortTypeSpecifier(SortType sortType) {
-        return switch (sortType) {
-            case PRICE_ASC -> new OrderSpecifier<>(Order.ASC, QFeed.feed.price);
-            case RECENT -> new OrderSpecifier<>(Order.DESC, QFeed.feed.id);
-            default -> new OrderSpecifier<>(Order.DESC, QFeed.feed.registeredUser.doubleValue().divide(QFeed.feed.maxUser.doubleValue()));
-        };
+    private OrderSpecifier<?>[] getSortTypeSpecifier(SortType sortType) {
+        if (sortType == PRICE_ASC) { // PRICE_ASC
+            return new OrderSpecifier[] {
+                new OrderSpecifier<>(Order.ASC, QFeed.feed.price),
+                new OrderSpecifier<>(Order.DESC, QFeed.feed.id)
+            };
+        } else { // RECENT
+            return new OrderSpecifier[] { new OrderSpecifier<>(Order.DESC, QFeed.feed.id) };
+        }
     }
 
     // 신청 가능 여부 필터링
