b10088fc5e69683c2911b689a9907ada6bb1869c
chungjeongsu <wjdwlgh2000@gmail.com>
2025-05-19 02:33:12 +0900

    feat: 리뷰 파일 이름 설정

--- src/main/java/com/ureca/juksoon/domain/review/entity/ReviewFile.java
+++ src/main/java/com/ureca/juksoon/domain/review/entity/ReviewFile.java
@@ -9,12 +9,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "review_file")
 @Builder
 @Getter
 @AllArgsConstructor
